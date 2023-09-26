1b07fd1c49ae8a54043519ffba60d868bcb5d1fb
Kazi Mehedy Hasan <mehedy19@gmail.com>
2023-09-26 18:01:13 +0600

    update

--- taskFive.php
+++ taskFive.php
@@ -35,25 +35,48 @@ echo $password;
 
 </pre>
 </div>
-
 <?php
-function generatePassword($length) {
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-    $password = '';
+// function generatePassword($length) {
+//     $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+//     $password = '';
 
-    for ($i = 0; $i < $length; $i++) {
-        $randomChar = $characters[rand(0, strlen($characters) - 1)];
-        $password .= $randomChar;
+//     for ($i = 0; $i < $length; $i++) {
+//         $randomChar = $characters[rand(0, strlen($characters) - 1)];
+//         $password .= $randomChar;
+//     }
+
+//     return $password;
+// }
+
+// // Generate and print a password with a length of 12 characters.
+// $password = generatePassword(12);
+// echo "<p>Your generated password is: <strong>$password</strong></p>";
+?>
+<form method="post">
+        <label for="password_length">Password Length:</label>
+        <input type="number" id="password_length" name="password_length" min="6" max="50" value="12"required>
+        <button type="submit" name="generate_password">Generate Password</button>
+    </form>
+
+    <?php
+    function generatePassword($length) {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+        $password = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomChar = $characters[rand(0, strlen($characters) - 1)];
+            $password .= $randomChar;
+        }
+
+        return $password;
     }
 
-    return $password;
-}
-
-// Generate and print a password with a length of 12 characters.
-$password = generatePassword(12);
-echo $password;
-?>
-
+    if (isset($_POST['generate_password'])) {
+        $password_length = isset($_POST['password_length']) ? (int)$_POST['password_length'] : 12;
+        $password = generatePassword($password_length);
+        echo "<p>Your generated password is: <strong>$password</strong></p>";
+    }
+    ?>
 
     </div>
 </div>
